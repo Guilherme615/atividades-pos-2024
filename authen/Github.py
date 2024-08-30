1d63c44520c677318161920ba022fac120d7a458
Guilherme <moura.guilherme@escolar.ifrn.edu.br>
2024-08-30 14:29:51 -0300

    Atividade do Github (WIP)

---
+++ authen/Github.py
@@ -0,0 +1,16 @@
+import requests
+from requests.auth import HTTPBasicAuth
+from getpass import getpass
+
+user = input("user: ")
+password = getpass("")
+  
+response = requests.get('https://api.github.com/user/followers', auth = HTTPBasicAuth('Guilherme615', password))
+  
+
+seguidores = response.json()
+
+for i, seguidor in enumerate(seguidores,):
+    print(f"{i} - {seguidor['login']}")
+
+print(response.status_code)
