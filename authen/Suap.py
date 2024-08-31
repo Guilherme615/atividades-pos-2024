d0c560de650dc970a1acd6871fbd72516040dd8d
Guilherme615 <moura.guilherme@escolar.ifrn.edu.br>
2024-08-31 19:56:24 -0300

    Feito

--- authen/Suap.py
+++ authen/Suap.py
@@ -1,16 +1,17 @@
 import requests
 from getpass import getpass
+import json
 
 api_url = "https://suap.ifrn.edu.br/api/"
 
-user = input("user: ")
-password = getpass()
+user = input("digite a matricula: ")
+password = getpass(prompt="senha: ")
 
 data = {"username":user,"password":password}
 
 response = requests.post(api_url+"v2/autenticacao/token/", json=data)
 token = response.json()["access"]
-print(response.json())
+#print(response.json())
 
 headers = {
     "Authorization": f'Bearer {token}'
@@ -18,7 +19,12 @@ headers = {
 
 print(headers)
 
-response = requests.get(api_url+"v2/minhas-informacoes/meus-dados/", headers=headers)
+ano_letivo = input("Digite o ano letivo: ")
+periodo = input("digite o periodo: ")
+response = requests.get(api_url+f"v2/minhas-informacoes/boletim/{ano_letivo}/{periodo}/", headers=headers).json()
 
-print(response.text)
-print(response)
+#print(response.text)
+#print(response)
+
+for disciplina in response:
+    print(f"Média {disciplina['mediadadisciplina']} - {disciplina['disciplina']}")
