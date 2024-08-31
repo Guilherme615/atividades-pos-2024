86b6b0e738119a19d020f9db41659d80411ddba6
Guilherme615 <moura.guilherme@escolar.ifrn.edu.br>
2024-08-31 19:58:59 -0300

    Feito

--- authen/Suap.py
+++ authen/Suap.py
@@ -4,27 +4,32 @@ import json
 
 api_url = "https://suap.ifrn.edu.br/api/"
 
-user = input("digite a matricula: ")
-password = getpass(prompt="senha: ")
+user = input("Digite a matrícula: ")
+password = getpass(prompt="Senha: ")
 
-data = {"username":user,"password":password}
+data = {"username": user, "password": password}
 
-response = requests.post(api_url+"v2/autenticacao/token/", json=data)
-token = response.json()["access"]
-#print(response.json())
+response = requests.post(api_url + "v2/autenticacao/token/", json=data)
+
+if response.status_code == 200:
+    token = response.json().get("access")
+    print("Autenticação bem-sucedida!")
+else:
+    print(f"Falha na autenticação: {response.status_code} - {response.text}")
+    exit()
 
 headers = {
     "Authorization": f'Bearer {token}'
 }
 
-print(headers)
-
 ano_letivo = input("Digite o ano letivo: ")
-periodo = input("digite o periodo: ")
-response = requests.get(api_url+f"v2/minhas-informacoes/boletim/{ano_letivo}/{periodo}/", headers=headers).json()
+periodo = input("Digite o período: ")
 
-#print(response.text)
-#print(response)
+response = requests.get(api_url + f"v2/minhas-informacoes/boletim/{ano_letivo}/{periodo}/", headers=headers)
 
-for disciplina in response:
-    print(f"Média {disciplina['mediadadisciplina']} - {disciplina['disciplina']}")
+if response.status_code == 200:
+    boletim = response.json()
+    for disciplina in boletim:
+        print(f"Média {disciplina['media_final']} - {disciplina['disciplina']}")
+else:
+    print(f"Erro ao obter o boletim: {response.status_code} - {response.text}")
